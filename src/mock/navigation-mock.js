5ceaf5d35aad3924a0ba4284527baed559fb8515
Denis <serzhantov-02@mail.ru>
2023-04-04 14:26:46 +0500

    add mocks for sort, filter, navs

---
+++ src/mock/navigation-mock.js
@@ -0,0 +1,3 @@
+
+
+export const navigations = ['Table', 'Stats'];
--- src/view/navigation-view.js
+++ src/view/navigation-view.js
@@ -1,19 +1,27 @@
 import AbstractView from '../framework/view/abstract-view';
 
-const createNavigationTemplate = () => (
+const createNavigation = (nav) => (`
+      <a class="trip-tabs__btn" href="#">${nav}</a>
+`);
+
+const createNavigationTemplate = (navigations) => (
   `
   <div class="trip-controls__navigation">
     <h2 class="visually-hidden">Switch trip view</h2>
     <nav class="trip-controls__trip-tabs  trip-tabs">
-      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-      <a class="trip-tabs__btn" href="#">Stats</a>
+    ${navigations.map((nav) => createNavigation(nav)).join('')}
     </nav>
   </div>
   `
 );
 
 export default class NavigationView extends AbstractView {
+  constructor(navigations) {
+    super();
+    this.navigations = navigations;
+  }
+
   get template() {
-    return createNavigationTemplate();
+    return createNavigationTemplate(this.navigations);
   }
 }
